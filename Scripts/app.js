d9aca8295f6663ceb32c49c4c456304879651d80
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-30 18:43:00 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+// IIFE - Immediately Invoked Function Expression
+"use strict";
+(function() {
+
+    let myVariable = 10;
+    console.log("my number is " + myVariable);
+
+    for (let index = 0; index < myVariable; index++) {
+        console.log("your number is " + index);
+    }
+
+    console.log(index);
+
+})();
